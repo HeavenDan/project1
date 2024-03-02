8529f87728adc8bbe789bcef737450bf089a2b8b
Kostiantyn <konstantinbelenko36@gmail.com>
2024-03-02 19:54:33 +0200

    Changed ping reply message

--- main.ts
+++ main.ts
@@ -1,41 +1,41 @@
-import { Client, GatewayIntentBits } from 'discord.js';
-import { REST, Routes } from 'discord.js';
+import { Client, GatewayIntentBits } from "discord.js";
+import { REST, Routes } from "discord.js";
 
-const CLIENT_ID:string="738073415948304407"
-const TOKEN:string="NzM4MDczNDE1OTQ4MzA0NDA3.GA4bqV.Tr4Xcy3HcLQJdDFiGn2xqHIX_bOSCa_TbiTvX0";
+const CLIENT_ID: string = "738073415948304407";
+const TOKEN: string =
+  "NzM4MDczNDE1OTQ4MzA0NDA3.GA4bqV.Tr4Xcy3HcLQJdDFiGn2xqHIX_bOSCa_TbiTvX0";
 const commands = [
   {
-    name: 'ping',
-    description: 'Replies with Pong!',
+    name: "ping",
+    description: "ЭэЭэЭ бля!",
   },
 ];
 const main = async () => {
+  const rest = new REST({ version: "10" }).setToken(TOKEN);
 
-    const rest = new REST({ version: '10' }).setToken(TOKEN);
-    
-    try {
-      console.log('Started refreshing application (/) commands.');
-    
-      await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
-    
-      console.log('Successfully reloaded application (/) commands.');
-    } catch (error) {
-      console.error(error);
+  try {
+    console.log("Started refreshing application (/) commands.");
+
+    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
+  }
+  const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+  client.on("ready", () => {
+    console.log(`Logged in as ${client.user!.tag}!`);
+  });
+
+  client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === "ping") {
+      await interaction.reply("Pong!");
     }
-    const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-    
-    client.on('ready', () => {
-      console.log(`Logged in as ${client.user!.tag}!`);
-    });
-    
-    client.on('interactionCreate', async interaction => {
-      if (!interaction.isChatInputCommand()) return;
-    
-      if (interaction.commandName === 'ping') {
-        await interaction.reply('Pong!');
-      }
-    });
-    client.login(TOKEN);
-}
+  });
+  client.login(TOKEN);
+};
 
-main()
+main();
